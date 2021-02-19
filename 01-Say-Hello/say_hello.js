44338363a6954f3742e345e2ea7e483220f9a378
Joshua Udeh <udehasilo@gmail.com>
2021-02-19 13:26:46 +0100

    exercise 1

--- 01-Say-Hello/say_hello.js
+++ 01-Say-Hello/say_hello.js
@@ -2,7 +2,15 @@
 /*
 
 here
-
-*/
+create a variable of name
+set variable of name to prompt a greeting text: What is your name please ?
+create a text list with both the greeting and name
+print the text greeting and greeting
+*/ 
 
 //insert your code below
+var name2;
+
+
+name2 = window.prompt('What is your name please ?');
+window.alert('welcome ' + String(name2));
