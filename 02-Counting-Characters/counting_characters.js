980b49b36552709b0f91b2d2c7b553f0beb3b375
Joshua Udeh <udehasilo@gmail.com>
2021-02-19 14:24:51 +0100

    exercise two

--- 02-Counting-Characters/counting_characters.js
+++ 02-Counting-Characters/counting_characters.js
@@ -2,7 +2,15 @@
 /*
 
 here
+first set the variable string
+set string to prompt the input string 
+print the lenght of string
 
 */
 
 //insert your code below
+var string;
+
+
+string = window.prompt('abc');
+window.alert(string.length);
